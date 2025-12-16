33856177cf46fcc920f07f69975c64f2e339256d
Murali krishna <gmksnd@gmail.com>
2025-12-16 13:29:08 +0530

    Update list-user.sh

--- beginner/list-user.sh
+++ beginner/list-user.sh
@@ -40,3 +40,30 @@ function list_users_with_read_access {
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
 list_users_with_read_access
+
+
+###########################################################################################################################################################################
+
+
+#!/bin/bash
+
+# Usage: ./list-read-users.sh OWNER REPO
+# Example: ./list-read-users.sh Murali-Hub1 Shell-scripts
+
+OWNER="$1"
+REPO="$2"
+
+# Ask for username and token if not already set
+if [[ -z "$GITHUB_USER" ]]; then
+  read -p "GitHub username: " GITHUB_USER
+fi
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  read -s -p "GitHub token: " GITHUB_TOKEN
+  echo
+fi
+
+echo "Users with read access to $OWNER/$REPO:"
+curl -s -u "$GITHUB_USER:$GITHUB_TOKEN" \
+  "https://api.github.com/repos/$OWNER/$REPO/collaborators" |
+jq -r '.[] | select(.permissions.pull == true) | .login'
